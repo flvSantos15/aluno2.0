5f08878f629f3312c89d66818d0688d8e8e6bd8b
flvsantos15 <flvsantos300@gmail.com>
2024-09-28 08:19:07 -0300

    feat: add product repository

--- src/module/store-catalog/repository/product.repository.spec.ts
+++ src/module/store-catalog/repository/product.repository.spec.ts
@@ -1,5 +1,7 @@
 import { Sequelize } from 'sequelize-typescript'
 import ProductModel from './product.model'
+import ProductRepository from './product.repository'
+import Id from '../../@shared/domain/value-object/id.value-object'
 
 describe('ProductRepository test', () => {
   let sequelize: Sequelize
@@ -20,16 +22,19 @@ describe('ProductRepository test', () => {
     await sequelize.close()
   })
 
+  const id1 = new Id('1')
+  const id2 = new Id('2')
+
   it('should find all products', async () => {
     await ProductModel.create({
-      id: '1',
+      id: id1.id,
       name: 'Product 1',
       description: 'Description 1',
       salesPrice: 100
     })
 
     await ProductModel.create({
-      id: '2',
+      id: id2.id,
       name: 'Product 2',
       description: 'Description 2',
       salesPrice: 200
@@ -39,10 +44,10 @@ describe('ProductRepository test', () => {
     const products = await productRepository.findAll()
 
     expect(products.length).toBe(2)
-    expect(products[0].id).toBe('1')
+    expect(products[0].id.id).toBe('1')
     expect(products[0].name).toBe('Product 1')
     expect(products[0].description).toBe('Description 1')
-    expect(products[1].id).toBe('2')
+    expect(products[1].id.id).toBe('2')
     expect(products[1].name).toBe('Product 2')
     expect(products[1].description).toBe('Description 2')
   })
