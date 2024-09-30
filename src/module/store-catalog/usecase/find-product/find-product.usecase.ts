9479683be1fbb9697a7485994500a676685ba961
flvsantos15 <flvsantos300@gmail.com>
2024-09-30 10:20:56 -0300

    feat: add find product usecase

---
+++ src/module/store-catalog/usecase/find-product/find-product.usecase.ts
@@ -0,0 +1,19 @@
+import { ProductGateway } from '../../gateway/product.gateway'
+import { FindProductInputDto, FindProductOutputDto } from './find-product.dto'
+
+export default class FindProductsUsecase {
+  constructor(private productRepository: ProductGateway) {
+    this.productRepository = productRepository
+  }
+
+  async execute(input: FindProductInputDto): Promise<FindProductOutputDto> {
+    const product = await this.productRepository.find(input.id)
+
+    return {
+      id: product.id.id,
+      name: product.name,
+      description: product.description,
+      salesPrice: product.salesPrice
+    }
+  }
+}
