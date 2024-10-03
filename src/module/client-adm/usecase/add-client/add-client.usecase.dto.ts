3083111105d63dfeeba0f1480042523f44343ade
flvsantos15 <flvsantos300@gmail.com>
2024-10-03 07:28:39 -0300

    feat: add client adm

---
+++ src/module/client-adm/usecase/add-client/add-client.usecase.dto.ts
@@ -0,0 +1,14 @@
+export interface AddClientInputDto {
+  name: string
+  email: string
+  address: string
+}
+
+export interface AddClientOututDto {
+  id: string
+  name: string
+  email: string
+  address: string
+  createdAt: Date
+  updatedAt: Date
+}
---
+++ src/module/client-adm/usecase/add-client/add-client.usecase.spec.ts
@@ -0,0 +1,1 @@
+describe('AddClientUsecase test', () => {})
